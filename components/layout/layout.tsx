79007331816401502b45aae5f33d0cc20205221c
jbxxnn <jbxxnn6@gmail.com>
2025-08-26 19:23:15 +0100

    fix showheader

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -12,7 +12,7 @@ interface LayoutProps {
 export function Layout({ children, showHeader = true }: LayoutProps) {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
-      <Header />
+      {showHeader && <Header />}
       {children}
       <CartDrawer />
     </div>
