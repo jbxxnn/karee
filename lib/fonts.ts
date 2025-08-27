06b786043dd31f6f50c38573854828a55c184739
jbxxnn <jbxxnn6@gmail.com>
2025-08-27 14:43:33 +0100

    fix mobile

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -3,32 +3,32 @@ import localFont from 'next/font/local'
 export const ppEditorialNew = localFont({
   src: [
     {
-      path: '../node_modules/next/font/local/fonts/PPEditorialNew-Ultralight.woff2',
+      path: '../public/fonts/PPEditorialNew-Ultralight.woff2',
       weight: '200',
       style: 'normal',
     },
     {
-      path: '../node_modules/next/font/local/fonts/PPEditorialNew-UltralightItalic.woff2',
+      path: '../public/fonts/PPEditorialNew-UltralightItalic.woff2',
       weight: '200',
       style: 'italic',
     },
     {
-      path: '../node_modules/next/font/local/fonts/PPEditorialNew-Regular.woff2',
+      path: '../public/fonts/PPEditorialNew-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../node_modules/next/font/local/fonts/PPEditorialNew-Italic.woff2',
+      path: '../public/fonts/PPEditorialNew-Italic.woff2',
       weight: '400',
       style: 'italic',
     },
     {
-      path: '../node_modules/next/font/local/fonts/PPEditorialNew-Ultrabold.woff2',
+      path: '../public/fonts/PPEditorialNew-Ultrabold.woff2',
       weight: '800',
       style: 'normal',
     },
     {
-      path: '../node_modules/next/font/local/fonts/PPEditorialNew-UltraboldItalic.woff2',
+      path: '../public/fonts/PPEditorialNew-UltraboldItalic.woff2',
       weight: '800',
       style: 'italic',
     },
@@ -40,32 +40,32 @@ export const ppEditorialNew = localFont({
 export const ppMori = localFont({
   src: [
     {
-      path: '../node_modules/next/font/local/fonts/PPMori-Extralight.woff2',
+      path: '../public/fonts/PPMori-Extralight.woff2',
       weight: '200',
       style: 'normal',
     },
     {
-      path: '../node_modules/next/font/local/fonts/PPMori-ExtralightItalic.woff2',
+      path: '../public/fonts/PPMori-ExtralightItalic.woff2',
       weight: '200',
       style: 'italic',
     },
     {
-      path: '../node_modules/next/font/local/fonts/PPMori-Regular.woff2',
+      path: '../public/fonts/PPMori-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../node_modules/next/font/local/fonts/PPMori-RegularItalic.woff2',
+      path: '../public/fonts/PPMori-RegularItalic.woff2',
       weight: '400',
       style: 'italic',
     },
     {
-      path: '../node_modules/next/font/local/fonts/PPMori-SemiBold.woff2',
+      path: '../public/fonts/PPMori-SemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../node_modules/next/font/local/fonts/PPMori-SemiBoldItalic.woff2',
+      path: '../public/fonts/PPMori-SemiBoldItalic.woff2',
       weight: '600',
       style: 'italic',
     },
