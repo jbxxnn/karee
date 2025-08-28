b31c820266e0f9a4dd0fac3e9f6a2322f410043e
jbxxnn <jbxxnn6@gmail.com>
2025-08-28 06:23:35 +0100

    production

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { About } from "@/components/about";
 import { Separator } from "@/components/ui/separator";
 import { ProductHome } from "@/components/product-home";
 import { WhyKaree } from "@/components/why-karee";
+import { InfiniteMovingCardsDemo } from "@/components/testimonials";
 
 export default function Home() {
   return (
@@ -37,6 +38,7 @@ export default function Home() {
                   <p className="text-sm font-pp-mori mb-12 w-[100%] lg:w-[30%] mx-auto">Experience whipped shea butter that melts effortlessly, leaving skin deeply nourished and naturally radiant.</p>
                 </div>
             <WhyKaree />
+            <InfiniteMovingCardsDemo />
             </div>
             <main className="flex-1 flex flex-col gap-6 px-4">
               <h2 className="font-medium text-xl mb-4">Next steps</h2>
