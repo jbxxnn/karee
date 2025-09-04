4c0961b42db06fa82dc467f9c219179242e892df
jbxxnn <jbxxnn6@gmail.com>
2025-09-04 15:43:07 +0100

    push

--- components/about.tsx
+++ components/about.tsx
@@ -1,21 +1,23 @@
 import Image from "next/image";
+import { ImageSlider } from "./image-slider";
 
 export function About() {
   return (
-    <div className="flex flex-col sm:flex-row gap-2 items-center h-auto md:h-[30rem] w-full p-3 md:p-5">
+    <div className="flex flex-col sm:flex-row gap-2 items-center h-auto md:h-[30rem] w-full p-3 md:p-5 overflow-hidden">
         <div className="flex flex-col gap-2 items-start w-[100%] md:w-[65%]">
             <div className="flex flex-col gap-2 w-[100%] md:w-[70%] mb-4">
                 <p className="text-md text-center md:text-left font-pp-mori mb-2">Kareè was born from a passion to share Africa’s timeless skincare treasure with the world. Shea butter has been trusted for generations, and we’ve reimagined it into a modern, whipped form that feels light, indulgent, and effortlessly nourishing.</p>
                 <h2 className="text-lg font-pp-mori text-sm font-bold text-center md:text-left">• About Kareè.</h2>
             </div>
             <div className="h-[30%] md:h-[100%] overflow-hidden">
-            <div className="flex flex-col gap-2 w-[100%] md:w-[50%] items-center justify-end h-full">
-                <Image 
+            <div className="flex flex-col gap-2 w-[100%] md:w-[50%] items-center justify-end h-full overflow-hidden">
+                {/* <Image 
                 src="/home-1-3.jpg" 
                 alt="About Kareè" 
                 width={500}
                 height={500}
-                />
+                /> */}
+                  <ImageSlider />
             </div>
             </div>
         </div>
---
+++ components/image-slider.tsx
@@ -0,0 +1,51 @@
+import { InfiniteSlider } from '@/components/ui/infinite-slider';
+import Image from 'next/image';
+
+export function ImageSlider() {
+  return (
+    <InfiniteSlider speedOnHover={20} gap={24} speed={100}>
+      <Image
+        src='https://i.scdn.co/image/ab67616d00001e02ad24c5e36ddcd1957ad35677'
+        alt='Dean blunt - Black Metal 2'
+        className='aspect-square w-[300px] rounded-[4px]'
+        width={300}
+        height={300}
+      />
+      <Image
+        src='https://i.scdn.co/image/ab67616d00001e02af73f776b92d4614152fb141'
+        alt='Jungle Jack - JUNGLE DES ILLUSIONS VOL 2'
+        className='aspect-square w-[300px] rounded-[4px]'
+        width={300}
+        height={300}
+      />
+      <Image
+        src='https://i.scdn.co/image/ab67616d00001e02ecdb8f824367a53468100faf'
+        alt='Yung Lean - Stardust'
+        className='aspect-square w-[300px] rounded-[4px]'
+        width={300}
+        height={300}
+      />
+      <Image
+        src='https://i.scdn.co/image/ab67616d00001e021624590458126fc8b8c64c2f'
+        alt='Lana Del Rey - Ultraviolence'
+        className='aspect-square w-[300px] rounded-[4px]'
+        width={300}
+        height={300}
+      />
+      <Image
+        src='https://i.scdn.co/image/ab67616d00001e020dcf0f3680cff56fe5ff2288'
+        alt='A$AP Rocky - Tailor Swif'
+        className='aspect-square w-[300px] rounded-[4px]'
+        width={300}
+        height={300}
+      />
+      <Image
+        src='https://i.scdn.co/image/ab67616d00001e02bc1028b7e9cd2b17c770a520'
+        alt='Midnight Miami (feat Konvy) - Nino Paid, Konvy'
+        className='aspect-square w-[300px] rounded-[4px]'
+        width={300}
+        height={300}
+      />
+    </InfiniteSlider>
+  );
+}
